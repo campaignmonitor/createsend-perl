53f81507ff6229775272b84bbbba6957ee998899
James Dennes <jdennes@gmail.com>
2013-05-08 13:38:59 +0200

    Version v2.2.1.

--- lib/Net/CampaignMonitor.pm
+++ lib/Net/CampaignMonitor.pm
@@ -11,7 +11,7 @@ use Carp;
 use URI;
 use URI::Escape;
 
-use version; our $VERSION = version->declare("v2.2.0");
+use version; our $VERSION = version->declare("v2.2.1");
 our $CAMPAIGN_MONITOR_DOMAIN = 'api.createsend.com';
 
 sub authorize_url {
@@ -1283,7 +1283,7 @@ Net::CampaignMonitor - A Perl wrapper for the Campaign Monitor API.
 
 =head1 VERSION
 
-This documentation refers to version v2.2.0.
+This documentation refers to version v2.2.1.
 
 =head1 SYNOPSIS
 
